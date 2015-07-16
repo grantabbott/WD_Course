17bb97923dcce68b9407f7ac747c8adc5e0972a9
grantabbott <mrgrantabbott@hotmail.com>
2015-07-15 21:34:44 -0700

    Starting off with PHP training

---
+++ PHP/php_intro.php
@@ -0,0 +1,22 @@
+<?php
+
+  //Simple print command
+  print 'hello world <br />';
+  echo 'This is also PHP <br />';
+
+
+  //Look into php.ini to setup troubleshooting and logging to the screen.
+
+
+  //Variables in php
+  $test = "I'm a variable <br />";
+
+  $test2 = "Me too! <br />";
+
+  $test3 = 75;
+
+  echo $test.$test2;
+
+  echo ($test3+42)/2+17;
+
+?>
